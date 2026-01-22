301e348f1e282ea152c1011b942d5315655f15f0
Dennis Chandrian <53209851+Dennis57@users.noreply.github.com>
2026-01-22 21:30:42 +1100

    Hello World
    Setup project

---
+++ ChadformFitness/app/src/main/java/com/example/chadformfitness/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.chadformfitness
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.chadformfitness.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    // Need to implement this if using data binding or viewbinding
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = binding.rvWorkoutSessionList
+
+        val numberOfColumns = 2
+
+        recyclerView.layoutManager =
+            GridLayoutManager(this, numberOfColumns)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cl_main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
