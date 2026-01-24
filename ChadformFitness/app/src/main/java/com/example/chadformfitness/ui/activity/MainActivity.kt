cc519270fa5701ffd0b80edd59e199d8d8369ae9
Dennis Chandrian <53209851+Dennis57@users.noreply.github.com>
2026-01-24 23:20:18 +1100

    Switch between menus in main activity
    Using simple fragment transaction

--- ChadformFitness/app/src/main/java/com/example/chadformfitness/ui/activity/MainActivity.kt
+++ ChadformFitness/app/src/main/java/com/example/chadformfitness/ui/activity/MainActivity.kt
@@ -7,9 +7,13 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.example.chadformfitness.R
 import com.example.chadformfitness.databinding.ActivityMainBinding
+import com.example.chadformfitness.ui.fragment.AccountFragment
+import com.example.chadformfitness.ui.fragment.CalendarFragment
 import com.example.chadformfitness.ui.fragment.HomeFragment
+import com.example.chadformfitness.ui.fragment.WellnessFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,16 +24,76 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // TODO: Learn best practice of switching between fragments
+
         // Create fragment
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
+                setCustomAnimations(
+                    R.anim.slide_in, // enter
+                    R.anim.fade_out, // exit
+                    R.anim.fade_in, // popEnter
+                    R.anim.slide_out // popExit
+                )
                 setReorderingAllowed(true)
-                add<HomeFragment>(R.id.fcv_main)
+                replace<HomeFragment>(R.id.fcv_main)
             }
         }
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding.tvHome.setOnClickListener {
+            supportFragmentManager.commit {
+                setCustomAnimations(
+                    R.anim.slide_in, // enter
+                    R.anim.fade_out, // exit
+                    R.anim.fade_in, // popEnter
+                    R.anim.slide_out // popExit
+                )
+                setReorderingAllowed(true)
+                replace<HomeFragment>(R.id.fcv_main)
+            }
+        }
+
+        binding.tvCalendar.setOnClickListener {
+            supportFragmentManager.commit {
+                setCustomAnimations(
+                    R.anim.slide_in, // enter
+                    R.anim.fade_out, // exit
+                    R.anim.fade_in, // popEnter
+                    R.anim.slide_out // popExit
+                )
+                setReorderingAllowed(true)
+                replace<CalendarFragment>(R.id.fcv_main)
+            }
+        }
+
+        binding.tvWellness.setOnClickListener {
+            supportFragmentManager.commit {
+                setCustomAnimations(
+                    R.anim.slide_in, // enter
+                    R.anim.fade_out, // exit
+                    R.anim.fade_in, // popEnter
+                    R.anim.slide_out // popExit
+                )
+                setReorderingAllowed(true)
+                replace<WellnessFragment>(R.id.fcv_main)
+            }
+        }
+
+        binding.tvAccount.setOnClickListener {
+            supportFragmentManager.commit {
+                setCustomAnimations(
+                    R.anim.slide_in, // enter
+                    R.anim.fade_out, // exit
+                    R.anim.fade_in, // popEnter
+                    R.anim.slide_out // popExit
+                )
+                setReorderingAllowed(true)
+                replace<AccountFragment>(R.id.fcv_main)
+            }
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cl_main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
