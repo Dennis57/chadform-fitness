952c2498ad17df627995a47c48172353d66a858b
Dennis Chandrian <53209851+Dennis57@users.noreply.github.com>
2026-02-06 17:51:27 +1100

    Changed bottom nav bar from linear layout to proper bottom navigation bar

--- ChadformFitness/app/src/main/java/com/example/chadformfitness/ui/activity/MainActivity.kt
+++ ChadformFitness/app/src/main/java/com/example/chadformfitness/ui/activity/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.example.chadformfitness.R
@@ -29,76 +30,85 @@ class MainActivity : AppCompatActivity() {
 
         // TODO: Learn best practice of switching between fragments
 
-        // Create fragment
+        // Create fragment if the instance is created for the first time
         if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                setCustomAnimations(
-                    R.anim.slide_in, // enter
-                    R.anim.fade_out, // exit
-                    R.anim.fade_in, // popEnter
-                    R.anim.slide_out // popExit
-                )
-                setReorderingAllowed(true)
-                replace<HomeFragment>(R.id.fcv_main)
-            }
+            replaceFragment(HomeFragment())
         }
 
-        binding.tvHome.setOnClickListener {
-            supportFragmentManager.commit {
-                setCustomAnimations(
-                    R.anim.slide_in, // enter
-                    R.anim.fade_out, // exit
-                    R.anim.fade_in, // popEnter
-                    R.anim.slide_out // popExit
-                )
-                setReorderingAllowed(true)
-                replace<HomeFragment>(R.id.fcv_main)
+        binding.bnvMain.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.botton_nav_home -> replaceFragment(HomeFragment())
+                R.id.bottom_nav_calendar -> replaceFragment(CalendarFragment())
+                R.id.bottom_nav_wellness -> replaceFragment(WellnessFragment())
+                R.id.bottom_nav_account -> replaceFragment(AccountFragment())
+                else -> {}
             }
+            // Return boolean True to register the item to the listener
+            true
         }
-
-        binding.tvCalendar.setOnClickListener {
-            supportFragmentManager.commit {
-                setCustomAnimations(
-                    R.anim.slide_in, // enter
-                    R.anim.fade_out, // exit
-                    R.anim.fade_in, // popEnter
-                    R.anim.slide_out // popExit
-                )
-                setReorderingAllowed(true)
-                replace<CalendarFragment>(R.id.fcv_main)
-            }
-        }
-
-        binding.tvWellness.setOnClickListener {
-            supportFragmentManager.commit {
-                setCustomAnimations(
-                    R.anim.slide_in, // enter
-                    R.anim.fade_out, // exit
-                    R.anim.fade_in, // popEnter
-                    R.anim.slide_out // popExit
-                )
-                setReorderingAllowed(true)
-                replace<WellnessFragment>(R.id.fcv_main)
-            }
-        }
-
-        binding.tvAccount.setOnClickListener {
-            supportFragmentManager.commit {
-                setCustomAnimations(
-                    R.anim.slide_in, // enter
-                    R.anim.fade_out, // exit
-                    R.anim.fade_in, // popEnter
-                    R.anim.slide_out // popExit
-                )
-                setReorderingAllowed(true)
-                replace<AccountFragment>(R.id.fcv_main)
-            }
-        }
+//        binding.tvHome.setOnClickListener {
+//            supportFragmentManager.commit {
+//                setCustomAnimations(
+//                    R.anim.slide_in, // enter
+//                    R.anim.fade_out, // exit
+//                    R.anim.fade_in, // popEnter
+//                    R.anim.slide_out // popExit
+//                )
+//                setReorderingAllowed(true)
+//                replace<HomeFragment>(R.id.fcv_main)
+//            }
+//        }
+//
+//        binding.tvCalendar.setOnClickListener {
+//            supportFragmentManager.commit {
+//                setCustomAnimations(
+//                    R.anim.slide_in, // enter
+//                    R.anim.fade_out, // exit
+//                    R.anim.fade_in, // popEnter
+//                    R.anim.slide_out // popExit
+//                )
+//                setReorderingAllowed(true)
+//                replace<CalendarFragment>(R.id.fcv_main)
+//            }
+//        }
+//
+//        binding.tvWellness.setOnClickListener {
+//            supportFragmentManager.commit {
+//                setCustomAnimations(
+//                    R.anim.slide_in, // enter
+//                    R.anim.fade_out, // exit
+//                    R.anim.fade_in, // popEnter
+//                    R.anim.slide_out // popExit
+//                )
+//                setReorderingAllowed(true)
+//                replace<WellnessFragment>(R.id.fcv_main)
+//            }
+//        }
+//
+//        binding.tvAccount.setOnClickListener {
+//            supportFragmentManager.commit {
+//                setCustomAnimations(
+//                    R.anim.slide_in, // enter
+//                    R.anim.fade_out, // exit
+//                    R.anim.fade_in, // popEnter
+//                    R.anim.slide_out // popExit
+//                )
+//                setReorderingAllowed(true)
+//                replace<AccountFragment>(R.id.fcv_main)
+//            }
+//        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cl_main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fcv_main, fragment)
+        fragmentTransaction.commit()
     }
 }
